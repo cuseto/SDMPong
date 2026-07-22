3201fc9e91cf0157efd6f725ff56d0bea8ea3aa8
Lowry02 <lorenzocusin02@gmail.com>
2026-07-22 17:47:42 +0200

    #5 - Removed unused import

--- app/src/test/java/com/cuseto/pong/AppTest.java
+++ app/src/test/java/com/cuseto/pong/AppTest.java
@@ -3,10 +3,8 @@
  */
 package com.cuseto.pong;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Disabled;
 
 class AppTest {
 
