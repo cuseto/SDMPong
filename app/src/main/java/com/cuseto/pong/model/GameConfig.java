7a2f59c44538152c3b67cede8ca366ba66dc10cf
Lowry02 <lorenzocusin02@gmail.com>
2026-07-22 18:05:18 +0200

    #5 - Changing x speed to see the bouncing effect

--- app/src/main/java/com/cuseto/pong/model/GameConfig.java
+++ app/src/main/java/com/cuseto/pong/model/GameConfig.java
@@ -69,7 +69,7 @@ public record GameConfig(   // For fixed things/rules
             80, 20, 4, 
             8, 100, 10, 80,
             300.0,
-            220.0, 140.0
+            20.0, 140.0
         );
     }
 
